1f8b691fc34aa6abb1432a946cce88f898f45ac1
Chin Riku <54855318+khanhdinh02@users.noreply.github.com>
2022-09-19 16:23:33 +0700

    create remote repo

---
+++ src/main/java/swp/medichor/MedichorApplication.java
@@ -0,0 +1,13 @@
+package swp.medichor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedichorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedichorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swp/medichor/MedichorApplicationTests.java
@@ -0,0 +1,13 @@
+package swp.medichor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedichorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
