e7490bb11a3ff2c1d3a254f790c341a4dee37273
Ngo Viet Tien <viettien1602@gmail.com>
2022-09-26 22:24:11 +0700

    add regex to test mail and phone

--- src/main/java/swp/medichor/utils/Validator.java
+++ src/main/java/swp/medichor/utils/Validator.java
@@ -1,13 +1,20 @@
 package swp.medichor.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
     public static boolean testEmail(String email) {
-        //regex to test mail
-        return true;
+        String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     public static boolean testPhoneNumber(String phone) {
-        //regex to test phoneNum
-        return true;
+        String regex = "^\\d{10}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phone);
+        return matcher.matches();
     }
 }
