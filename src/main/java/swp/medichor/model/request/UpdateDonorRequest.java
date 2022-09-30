bced37c9342c0de19104efe10831d23a753b5ed6
khanhdinh02 <khanhdinh02@yahoo.com>
2022-09-30 10:18:20 +0700

    remove dump dump code

--- src/main/java/swp/medichor/model/request/UpdateDonorRequest.java
+++ src/main/java/swp/medichor/model/request/UpdateDonorRequest.java
@@ -2,11 +2,8 @@ package swp.medichor.model.request;
 
 import java.time.LocalDate;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import swp.medichor.enums.Sex;
-import swp.medichor.model.Donor;
 
-@NoArgsConstructor
 @Getter
 public class UpdateDonorRequest {
 
@@ -18,15 +15,4 @@ public class UpdateDonorRequest {
     private String bloodType;
     private String anamnesis;
     private UpdateUserRequest user;
-
-    public UpdateDonorRequest(Donor donor) {
-        this.name = donor.getName();
-        this.birthday = donor.getBirthday();
-        this.sex = donor.getSex();
-        this.identityNum = donor.getIdentityNum();
-        this.avatar = donor.getAvatar();
-        this.bloodType = donor.getBloodType();
-        this.anamnesis = donor.getAnamnesis();
-        this.user = new UpdateUserRequest(donor.getUser());
-    }
 }
