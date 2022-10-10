0d8ec437c42156fda015bd35f7c427e02256740d
khanhdinh02 <khanhdinh02@yahoo.com>
2022-10-10 10:08:10 +0700

    delete (lock) user

--- src/main/java/swp/medichor/service/AdminService.java
+++ src/main/java/swp/medichor/service/AdminService.java
@@ -8,16 +8,23 @@ import swp.medichor.model.response.Response;
 import swp.medichor.repository.OrganizationRepository;
 import javax.transaction.Transactional;
 import java.util.Optional;
+import swp.medichor.enums.Role;
+import swp.medichor.repository.UserRepository;
 
 @Service
 public class AdminService {
+
     @Autowired
     private OrganizationRepository organizationRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Transactional
     public Response verifyOrganizationAccount(Integer id) {
         Optional<Organization> existOrganization = organizationRepository.findById(id);
-        if (existOrganization.isEmpty()) return new Response(400, false, "ID not found");
+        if (existOrganization.isEmpty()) {
+            return new Response(400, false, "ID not found");
+        }
         Organization organization = existOrganization.get();
         organization.setApprove(Approve.APPROVED);
         organization.getUser().setEnabled(true);
@@ -27,9 +34,25 @@ public class AdminService {
     @Transactional
     public Response rejectOrganizationAccount(Integer id) {
         Optional<Organization> existOrganization = organizationRepository.findById(id);
-        if (existOrganization.isEmpty()) return new Response(400, false, "ID not found");
+        if (existOrganization.isEmpty()) {
+            return new Response(400, false, "ID not found");
+        }
         Organization organization = existOrganization.get();
         organization.setApprove(Approve.REJECTED);
         return new Response(200, true, "Reject successfully");
+    }
+
+    public void lockUser(int userId) {
+        userRepository.findById(userId)
+                .ifPresentOrElse(user -> {
+                    if (user.getRole() != Role.ADMIN) {
+                        user.setStatus(false);
+                        userRepository.save(user);
+                    } else {
+                        throw new RuntimeException("Cannot lock user with role ADMIN");
+                    }
+                }, () -> {
+                    throw new RuntimeException("User ID not found");
+                });
     }
 }
