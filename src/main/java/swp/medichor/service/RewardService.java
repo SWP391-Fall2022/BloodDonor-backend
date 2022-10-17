1d80dec36d070145245b5df2cf116ce842f7224c
khanhdinh02 <54855318+khanhdinh02@users.noreply.github.com>
2022-10-17 18:12:34 +0700

    CRUD reward

--- src/main/java/swp/medichor/service/RewardService.java
+++ src/main/java/swp/medichor/service/RewardService.java
@@ -3,10 +3,12 @@ package swp.medichor.service;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import swp.medichor.model.Reward;
+import swp.medichor.model.request.RewardRequest;
 import swp.medichor.model.response.EarnedRewardResponse;
 import swp.medichor.model.response.RewardResponse;
 import swp.medichor.repository.EarnedRewardRepository;
@@ -51,7 +53,54 @@ public class RewardService {
                 && !earned.contains(reward))
                 .map(reward -> new RewardResponse(reward))
                 .collect(Collectors.toList());
-        
+
         return available;
+    }
+
+    public RewardResponse getById(int id) {
+        Optional<Reward> reward = rewardRepository.findById(id);
+        if (reward.isPresent()) {
+            return new RewardResponse(reward.get());
+        } else {
+            return null;
+        }
+    }
+
+    public int addReward(RewardRequest request) {
+        Reward newReward = rewardRepository.save(Reward.builder()
+                .expiredDate(request.getExpiredDate())
+                .level(request.getLevel())
+                .sponsor(request.getSponsor())
+                .code(request.getCode())
+                .status(request.getStatus())
+                .details(request.getDetails())
+                .amount(request.getAmount())
+                .build());
+        return newReward.getId();
+    }
+
+    public void updateReward(int id, RewardRequest request) {
+        rewardRepository.findById(id)
+                .ifPresentOrElse(reward -> {
+                    reward.setExpiredDate(request.getExpiredDate());
+                    reward.setLevel(request.getLevel());
+                    reward.setSponsor(request.getSponsor());
+                    reward.setCode(request.getCode());
+                    reward.setDetails(request.getDetails());
+                    reward.setAmount(request.getAmount());
+                    rewardRepository.save(reward);
+                }, () -> {
+                    throw new RuntimeException("Reward not found");
+                });
+    }
+
+    public void disableReward(int id) {
+        rewardRepository.findById(id)
+                .ifPresentOrElse(reward -> {
+                    reward.setStatus(false);
+                    rewardRepository.save(reward);
+                }, () -> {
+                    throw new RuntimeException("Reward not found");
+                });
     }
 }
