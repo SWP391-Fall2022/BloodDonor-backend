8311d11a3daf91b8f2fceafab163328d586ffaf4
viettien1602 <viettien1602@gmail.com>
2022-11-14 22:56:44 +0700

    change link of email

--- src/main/java/swp/medichor/utils/EmailPlatform.java
+++ src/main/java/swp/medichor/utils/EmailPlatform.java
@@ -7,7 +7,7 @@ import swp.medichor.model.User;
 
 public class EmailPlatform {
     private static final String DOMAIN = "http://localhost:3000/";
-    private static final String LINK = DOMAIN + "campaign/detailCampaign/";
+    private static final String LINK = DOMAIN + "campaign/campaign-detail/";
     private static final String OTHER_CAMPAIGNS_LINK = DOMAIN + "campaign";
     private static final String MEDICAL_LINK = DOMAIN + "donor/history";
 
